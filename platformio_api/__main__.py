f68d6c9742699984a7e04efe1c8496a218d706ed
Namo Aton <namoaton@gmail.com>
2018-02-10 20:44:53 +0200

    some  fixes

--- platformio_api/__main__.py
+++ platformio_api/__main__.py
@@ -21,7 +21,7 @@ from platformio_api import __version__, maintenance
 from platformio_api.database import sync_db
 from platformio_api.web import app
 from platformio_api import config
-import github_terrier
+from platformio_api import github_terrier
 
 
 @group()
@@ -100,14 +100,13 @@ def purge_cache():
 @argument('search_query', type=str, nargs=1)
 @argument('min_repo_stars', type=int, nargs=1)
 def githubterrier(search_query, min_repo_stars):
-    github_login = config['GITHUB_LOGIN']
-    github_password = config['GITHUB_PASSWORD']
-    gh_list = github_terrier.get_github_libs(search_query, github_login,
-                                             github_password, min_repo_stars)
-    pio_list = github_terrier.get_pio_libs()
-    new_found_libs = github_terrier.find_new_libs(gh_list, pio_list)
-    new_libs = github_terrier.check_libs(new_found_libs)
-    github_terrier.register_new_libs(new_libs)
+    github_terrier.register_new_libs(
+        github_terrier.check_libs(
+            github_terrier.find_new_libs(
+                github_terrier.get_github_libs(
+                    search_query, config['GITHUB_LOGIN'],
+                    config['GITHUB_PASSWORD'], min_repo_stars),
+                github_terrier.get_pio_libs())))
 
 
 def main():
