ae3ffebeed90c01a69ac430dc80379df6d453a95
Namo Aton <namoaton@gmail.com>
2018-02-14 20:13:01 +0200

    add copyright

--- platformio_api/github_terrier.py
+++ platformio_api/github_terrier.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 import os
 import shutil
@@ -56,7 +70,7 @@ class GithubTerrier(object):
             return
         manifest_url = self._maybe_manifest_url(repository)
         if not manifest_url:
-            return 
+            return
         self.register_manifest(manifest_url)
 
     def register_manifest(self, manifest_url):
