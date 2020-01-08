818cfac5442a6bb70dea75089d40857fe6f16139
Ivan Kravets <me@ikravets.com>
2020-01-08 21:36:53 +0200

    Fix issue with symlinks

--- platformio_api/vcsclient.py
+++ platformio_api/vcsclient.py
@@ -87,7 +87,7 @@ class VCSBaseClient(object):
                         copytree(
                             item_path,
                             join(destination_dir, item),
-                            symlinks=False)
+                            symlinks=True)
                 rmtree(subdir)
         finally:
             remove(arch_path)
@@ -124,7 +124,7 @@ class GitVCSClient(VCSBaseClient):
     def clone(self, destination_dir):
         if isdir(destination_dir):
             rmtree(destination_dir)
-        copytree(self._repo.working_tree_dir, destination_dir, symlinks=False)
+        copytree(self._repo.working_tree_dir, destination_dir, symlinks=True)
         rmtree(join(destination_dir, ".git"))
 
     def get_last_commit(self, path=None):
